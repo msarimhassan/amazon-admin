0b9c7610c3d86acb683333e54acf9c05e9351bb8
sarim-dev <sarimhassan5@gmail.com>
2022-07-14 13:01:51 +0500

    Integrate order status API

--- src/pages/Orders/Table/TableRow.js
+++ src/pages/Orders/Table/TableRow.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Button } from 'reactstrap';
-
+import { Network, Urls, config } from '../../../config';
+import { toast } from 'react-toastify';
 export default function ({id,name,quantity}) {
 
+    const handleStatus=async(id) => {
+        const response = await Network.put(Urls.updateOrder + id, {}, (await config()).headers);
+        console.log(response);
+        if (!response.ok)
+        {
+            return toast.error(response.data.error, { position: 'top-right' });
+        }
+        toast.success(response.data.message, { position: "top-right" });
+    }
     return (
         <>
             <tr>
@@ -12,7 +22,7 @@ export default function ({id,name,quantity}) {
                    {quantity}
                 </td>
                 <td>
-                    <Button className='mx-2' color='primary' >
+                    <Button className='mx-2' color='primary' onClick={()=>handleStatus(id)} >
                         Delivered
                     </Button>
                   
