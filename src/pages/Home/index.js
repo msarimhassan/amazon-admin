3804656237ffde8e109f06d78bb165c322cb856c
sarim-dev <sarimhassan5@gmail.com>
2022-07-20 17:03:08 +0500

    Add One Signal on login

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,14 +3,17 @@ import Stats from './Stats';
 import RevenueReport from './RevenueReport';
 import { useNavigate } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
+import useUser from '../../hooks/useUser';
 import OneSignalReact from 'react-onesignal';
 export default function HomePage() {
-    const {token } = useAuth();
+    const { token } = useAuth();
+    const { userData} = useUser();
     const navigate = useNavigate();
      useEffect(() => {
          OneSignalReact.init({
              appId: '78320a06-9037-4f58-acfc-227f3f07f141',
          });
+         OneSignalReact.setExternalUserId(userData._id);
      }, []);
     return (
         <>
